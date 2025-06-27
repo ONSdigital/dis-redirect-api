401df34db49f5a92d3f07b00b9a8a2f502d7e719
Ighomena Odebala <ighomena.odebala@ons.gov.uk>
2025-06-27 16:29:54 +0100

    Add two layer of validating key

--- api/redirects.go
+++ api/redirects.go
@@ -18,11 +18,18 @@ func (api *RedirectAPI) getRedirect(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	redirectID := vars["id"]
 
-	decodedKey, err := decodeBase64(redirectID)
-	if err != nil {
-		api.handleError(ctx, w, err, http.StatusBadRequest)
+	if !isValidBase64(redirectID) {
+		http.Error(w, "invalid base64 id", http.StatusBadRequest)
 		return
 	}
+
+	decodedString, err := base64.URLEncoding.DecodeString(redirectID)
+	if err != nil {
+		http.Error(w, "cannot decode id", http.StatusBadRequest)
+		return
+	}
+
+	decodedKey := string(decodedString)
 
 	redirect, err := api.Store.GetRedirect(ctx, decodedKey)
 	if err != nil {
@@ -51,12 +58,7 @@ func (api *RedirectAPI) getRedirect(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// decodeBase64 returns the original string of a base64 encoded string
-func decodeBase64(encodedKey string) (string, error) {
-	decodedKey, err := base64.StdEncoding.DecodeString(encodedKey)
-	if err != nil {
-		return "", fmt.Errorf("key %s not base64", encodedKey)
-	}
-
-	return string(decodedKey), nil
+func isValidBase64(s string) bool {
+	_, err := base64.StdEncoding.DecodeString(s)
+	return err == nil
 }
