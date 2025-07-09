37772df90903faf2cfc49063de0d0899ad894da5
cookel2 <eleanor.cook@ons.gov.uk>
2025-07-09 14:30:09 +0100

    fix lint errors in Redirects java class

--- sdk/java/src/main/java/com/github/onsdigital/dis/redirect/api/sdk/model/Redirects.java
+++ sdk/java/src/main/java/com/github/onsdigital/dis/redirect/api/sdk/model/Redirects.java
@@ -5,10 +5,12 @@ import lombok.Setter;
 import java.util.ArrayList;
 
 /**
- * The model of a redirects object, which contains a list of redirect objects, as provided by the redirect API.
+ * The model of a redirects object, which contains a list of redirect objects,
+ * as provided by the redirect API.
  */
 public class Redirects {
-    /** The approximate number of redirects requested, defaulted to 10 and limited to 1000. */
+    /** The approximate number of redirects requested,
+     *  defaulted to 10 and limited to 1000. */
     @Getter
     @Setter
     private int count;
@@ -18,7 +20,8 @@ public class Redirects {
     @Setter
     private ArrayList<Redirect> redirectList;
 
-    /** The cursor value returned from a previous response. 0 should be used for the first request. */
+    /** The cursor value returned from a previous response.
+     * 0 should be used for the first request. */
     @Getter
     @Setter
     private String cursor;
@@ -26,12 +29,12 @@ public class Redirects {
     /** The cursor value to use in the next request. */
     @Getter
     @Setter
-    private String next_cursor;
+    private String nextCursor;
 
     /** The total number of redirects in the store. */
     @Getter
     @Setter
-    private int total_count;
+    private int totalCount;
 
     /**
      * Default no-argument constructor.
@@ -43,17 +46,20 @@ public class Redirects {
     /**
      * Constructs a new Redirects object with the specified values.
      *
-     * @param count - the number of redirects requested
-     * @param redirectList - the list of Redirect objects
-     * @param cursor - the position to start counting from
-     * @param next_cursor - the cursor value to use in the next request
-     * @param total_count - the total number of redirects in the store
+     * @param initCount - initialises the number of redirects requested
+     * @param initRedirectList - the list of Redirect objects
+     * @param initCursor - the position to start counting from
+     * @param initNextCursor - the cursor value to use in the next request
+     * @param initTotalCount - the total number of redirects in the store
      */
-    public Redirects(final int count, final ArrayList<Redirect> redirectList, String cursor, String next_cursor, int total_count) {
-        this.count = count;
-        this.redirectList = redirectList;
-        this.cursor = cursor;
-        this.next_cursor = next_cursor;
-        this.total_count = total_count;
+    public Redirects(final int initCount,
+                     final ArrayList<Redirect> initRedirectList,
+                     final String initCursor, final String initNextCursor,
+                     final int initTotalCount) {
+        this.count = initCount;
+        this.redirectList = initRedirectList;
+        this.cursor = initCursor;
+        this.nextCursor = initNextCursor;
+        this.totalCount = initTotalCount;
     }
 }
