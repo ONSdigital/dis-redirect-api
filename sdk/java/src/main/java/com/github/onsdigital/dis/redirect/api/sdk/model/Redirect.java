86cb545706834611109e078ece25719332d3bd3d
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2025-08-21 09:03:54 +0100

    Consolidate id generation in java sdk

--- sdk/java/src/main/java/com/github/onsdigital/dis/redirect/api/sdk/model/Redirect.java
+++ sdk/java/src/main/java/com/github/onsdigital/dis/redirect/api/sdk/model/Redirect.java
@@ -1,5 +1,9 @@
 package com.github.onsdigital.dis.redirect.api.sdk.model;
 
+import java.util.Base64;
+
+import org.apache.commons.lang3.StringUtils;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,6 +21,11 @@ public class Redirect {
     @Setter
     private String to;
 
+    /** The id - base64 encoded version of from. */
+    @Getter
+    @Setter
+    private String id;
+
     /**
      * Default no-argument constructor.
      */
@@ -31,7 +40,34 @@ public class Redirect {
      * @param toPath   the target path
      */
     public Redirect(final String fromPath, final String toPath) {
+        this(fromPath, toPath, Redirect.encodeRedirectID(fromPath));
+    }
+
+    /**
+     * Constructs a new Redirect with the specified from and to paths.
+     *
+     * @param fromPath  the source path
+     * @param toPath    the target path
+     * @param encodedId the id of the redirect
+     */
+    public Redirect(final String fromPath, final String toPath,
+            final String encodedId) {
         this.from = fromPath;
         this.to = toPath;
+        this.id = encodedId;
+    }
+
+    /**
+     * Encodes a string as a redirect ID - consolidated functionality
+     * to keep encoding in one place for consistency.
+     *
+     * @param str
+     * @return base64 encoded string
+     */
+    public static String encodeRedirectID(final String str) {
+        if (StringUtils.isNotEmpty(str)) {
+            return Base64.getEncoder().encodeToString(str.getBytes());
+        }
+        return "";
     }
 }
